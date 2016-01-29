fe6660c0c7037e321c6849fd0a2a139773aa3805
Frank Carey <frank@devinci.io>
2016-01-29 15:08:01 -0500

    Remove all the specific hhs and behat stuff from dkan_workflow so that dkan and ahoy are used instead.

--- test/circle-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Make sure an error during execution gets passed along to circleCI
-set -e
-# Do full installation.
-echo "===> Cached databases not configured. Installing from scratch."
-echo "drush si dkan --sites-subdir=default --db-url=$DATABASE_URL --account-name=admin --account-pass=admin  --site-name="DKAN Workflow QA" install_configure_form.update_status_module='array(FALSE,FALSE)' --yes"
-drush si dkan --sites-subdir=default --db-url=$DATABASE_URL --account-name=admin --account-pass=admin  --site-name="DKAN Workflow QA" install_configure_form.update_status_module='array(FALSE,FALSE)' --yes
-drush cc drush; drush cc all --yes
